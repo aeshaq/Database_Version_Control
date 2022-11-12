f0b9d4368fe00e89726e48ce8527b3cdb23eb678
Oussama Saoudi <oussama.sa216@gmail.com>
2022-11-11 16:17:54 -0800

    Removed filename from model representation, changed Commit types
    Removed filename from model representation since this is not
    present in the SQL database.
    
    Changed Commit model to take path to file instead of File object

--- src/model/Repository_Object.java
+++ src/model/Repository_Object.java
@@ -2,19 +2,13 @@ package model;
 
 public class Repository_Object {
     private final String repositoryName;
-    private final String File_name;
     private final String Path;
     private final int Size;
 
-    public Repository_Object(String repositoryName, String file_name, String path, int size) {
+    public Repository_Object(String repositoryName, String path, int size) {
         this.repositoryName = repositoryName;
-        this.File_name = file_name;
         this.Path = path;
         this.Size = size;
-    }
-
-    public String getFile_name() {
-        return File_name;
     }
 
     public String getPath() {
