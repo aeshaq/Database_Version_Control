a77a52eb1a52637f3b4946cc7d584b2997134350
Oussama Saoudi <oussama.sa216@gmail.com>
2022-11-21 12:01:15 -0800

    Fixed compilation errors

--- src/model/Directory.java
+++ src/model/Directory.java
@@ -1,14 +1,12 @@
 package model;
 
 public class Directory extends Repository_Object {
-    private final String Parent_directory_path;
 
-    public Directory(String name, String path, int size, String parent_dir_path) {
-        super(name, path, size);
-        this.Parent_directory_path = parent_dir_path;
+    public Directory( String repositoryName,
+                      String parent_path,
+                      String path,
+                      int size) {
+        super(repositoryName, parent_path, path, size);
     }
 
-    public String getParent_directory_path() {
-        return Parent_directory_path;
-    }
 }
--- src/model/File.java
+++ src/model/File.java
@@ -3,8 +3,8 @@ package model;
 public class File extends Repository_Object {
     private final String file_extension;
     private final String file_contents;
-    public File(String repository_name, String path, int size, String file_contents, String file_extension) {
-        super(repository_name, path, size);
+    public File(String repositoryName, String parent_path, String path, int size, String file_contents, String file_extension) {
+        super(repositoryName, parent_path, path, size);
         this.file_contents = file_contents;
         this.file_extension = file_extension;
     }
