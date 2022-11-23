60ab3740908dd2f46a391120ee872249f10aa202
oliox <oliox@students.cs.ubc.ca>
2022-11-23 12:31:48 -0800

    Grouped orgs sections into panels, added labels

--- src/ui/OrgsUI.java
+++ src/ui/OrgsUI.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -14,48 +15,67 @@ public class OrgsUI extends JPanel {
     private JScrollPane orgScrollPane;
     private JList megaList;
     private JScrollPane megaScrollPane;
-    private JTextArea megaTitle;
+    private JLabel megaTitle;
+    private JLabel lbel3;
+    private JLabel mostUsed;
     private JLabel aveCommits;
+
+    private JPanel beforePanel;
+    private JPanel afterPanel;
+
 
     public OrgsUI() {
         GridBagConstraints c = new GridBagConstraints();
         
         c.weightx = 5.0;
+        c.weighty = 5.0;
+        c.fill = GridBagConstraints.BOTH;
  
         this.setLayout(new GridBagLayout());
 
-        //No fill
-        aveCommits = new JLabel("Average number of commits per user: 10");
-        c.gridx = 3;
-        c.gridy = 1;
-        c.weightx = 2.0;
-        this.add(aveCommits, c);
-        c.weightx = 5.0;
+        beforePanel = buildBeforeSelectPanel();
+        afterPanel = buildAfterSelectPanel();
+        beforePanel.setBorder(new EmptyBorder(10, 10, 10, 20));
+        c.gridx = 0;
+        c.gridy = 0;
+        this.add(beforePanel, c);
+        c.gridx = 1;
+        c.gridy = 0;
+        this.add(afterPanel, c);
 
+
+       
+
+
+
+        addListener();
+    }
+
+    private JPanel buildBeforeSelectPanel() {
+        JPanel myPanel = new JPanel();
+        GridBagConstraints c = new GridBagConstraints();
+        myPanel.setLayout(new GridBagLayout());
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 5.0;
 
         enterButton = new Button("Enter");
         c.gridx = 0;
         c.gridy = 1;
 
-        this.add(enterButton, c);
+        myPanel.add(enterButton, c);
 
         addToUser = new Button("Add org to current user");
         c.gridx = 0;
-        c.gridy = 4;
-        this.add(addToUser, c);
+        c.gridy = 3;
+        myPanel.add(addToUser, c);
 
-        megaTitle = new JTextArea("Mega Users List");
-        c.gridx = 1;
-        c.gridy = 1;
-        megaTitle.setEditable(false);
-        this.add(megaTitle, c);
 
         //Horizontal fill
         c.fill = GridBagConstraints.HORIZONTAL;
         username = new JTextField("Search Organizations");
         c.gridx = 0;
         c.gridy = 0;
-        this.add(username, c);
+        myPanel.add(username, c);
 
         //Both fill
         c.fill = GridBagConstraints.BOTH;
@@ -66,20 +86,60 @@ public class OrgsUI extends JPanel {
         orgScrollPane= new JScrollPane(orgList);
         orgScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 2;
    
-        this.add(orgScrollPane, c);
+        myPanel.add(orgScrollPane, c);
+        return myPanel;
 
+    }
+
+    private JPanel buildAfterSelectPanel() {
+        JPanel myPanel = new JPanel();
+        GridBagConstraints c = new GridBagConstraints();
+        myPanel.setLayout(new GridBagLayout());
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 2.0;
+        
+        c.gridx = 0;
+
+        aveCommits = new JLabel("Average number of commits per user: 10");
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 2.0;
+        myPanel.add(aveCommits, c);
+
+        lbel3 = new JLabel("something else?");
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 2.0;
+        myPanel.add(lbel3, c);
+
+        mostUsed = new JLabel("Most commited repo: here");
+        c.gridx = 1;
+        c.gridy = 2;
+        c.weightx = 2.0;
+        myPanel.add(mostUsed, c);
+        c.weightx = 5.0;
+
+        c.fill = GridBagConstraints.BOTH;
+        
+        megaTitle = new JLabel("Mega Users List");
+        c.gridx = 0;
+        c.gridy = 0;
+        myPanel.add(megaTitle, c);
+
+        c.weighty = 5.0;
         String m[] = {"john",  "james ", " Jeremy",  "Jones", "jajajja"};
         megaList = new JList(m);
         megaScrollPane= new JScrollPane(megaList);
         megaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        c.gridx = 1;
+        c.gridx = 0;
         c.gridy = 3;
-        this.add(megaScrollPane, c);
+        myPanel.add(megaScrollPane, c);
 
-        addListener();
+        return myPanel;
     }
+
 
 
     protected void addListener() {
